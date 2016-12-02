315fd1b6e809a13483aaa1426d243b1f70defce5
sindresf <sindre.fjermestad@gmail.com>
2016-12-02 10:43:42 +0100

    the simple f# syntax introduction of chapter 1

--- ChapterOne/ChapterOne/Program.fs
+++ ChapterOne/ChapterOne/Program.fs
@@ -1,4 +1,9 @@
-﻿[<EntryPoint>]
+﻿open System
+
+[<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+    let x = "Hello World"
+    Console.WriteLine(x)
+    let y = Console.ReadKey()
     0 // return an integer exit code
--- ChapterOne/ChapterOne/Script1.fsx
+++ ChapterOne/ChapterOne/Script1.fsx
@@ -1,1 +1,16 @@
-﻿
+﻿let ints = [|1;2;3;4;5;6|]
+let summedValue = ints |> Array.sum
+let multiplied = ints |> Array.map (fun i -> i*2)
+let multiplyByTwo x =
+    x * 2
+let multiplied' = ints |> Array.map (fun i -> multiplyByTwo i)
+let isEven x = 
+    match x % 2 = 0 with
+    | true -> "even"
+    | false -> "odd"
+isEven 2
+isEven 3
+let multipliedAnIsEven =
+    ints
+    |> Array.map (fun i -> multiplyByTwo i)
+    |> Array.map (fun i -> isEven i)
