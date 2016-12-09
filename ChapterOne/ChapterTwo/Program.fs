504b8eff6ae8bf0a623d69db29bf1a6facd794c0
sindresf <sindre.fjermestad@gmail.com>
2016-12-09 11:03:55 +0100

    sjappter tou

---
+++ ChapterOne/ChapterTwo/Program.fs
@@ -0,0 +1,11 @@
+﻿type Point = 
+    | X
+
+type Shape =
+    | Rectangle of Point * Point
+    | Ellipse of Point * Point
+    | Composed of Shape * Shape
+
+match shape with
+| Rectangle(pfrom, pto) ->
+    rectangleArea
