de23671ba78c8cb198a3cd5548fde45ee6514441
sindresf <sindre.fjermestad@gmail.com>
2016-12-12 09:54:10 +0100

    different equal match-deconstructs

--- ChapterOne/ChapterTwo/Program.fs
+++ ChapterOne/ChapterTwo/Program.fs
@@ -16,3 +16,13 @@ let withItem2 newItem2 tuple =
 
 let old = ("old",1)
 let nw = withItem2 (snd(old) + 1) old
+
+let withItm2 newItem2 (origItem1, _) = (origItem1, newItem2)
+
+let newest = withItm2 12 nw
+
+let withAitum2 newIteme2 tuple =
+    match tuple with
+    | (origItem1, _) -> (origItem1, newIteme2)
+
+let future = withAitum2 1337 newest
